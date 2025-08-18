6ece8f09eae96b787f6b1b5e3afc1eeeeed55561
gcdvl <180088513+gcdvl@users.noreply.github.com.>
2025-08-18 18:55:49 +0530

    edit

--- rps.js
+++ rps.js
@@ -9,11 +9,7 @@ let humanscore=0;
 let cpscore=0;
 
 
-function get_human()
-{
-  let human_choice = prompt("Enter Rock , Paper or Scissors");
-  return human_choice;
-}
+
 function get_cp(){
   const randomNumber = getRandomIntInclusive(1, 3);
   if (randomNumber==1){
@@ -28,55 +24,55 @@ function get_cp(){
   }
 }
 
+
+     
+    const rockbtn = document.getElementById('rockbtn');
+    rockbtn.addEventListener("click",function(){
+          const cp_choice = get_cp();
+    playround(cp_choice,"rock")});
+    
+    const paperbtn = document.getElementById('paperbtn');
+    paperbtn.addEventListener("click",function(){
+      const cp_choice = get_cp();
+    playround(cp_choice,"paper")});
+
+    const scissorsbtn = document.getElementById('scissorsbtn');
+    scissorsbtn.addEventListener("click",function(){
+      const cp_choice = get_cp();
+    playround(cp_choice,"scissors")});
+    
+    
+let round=0;
+
 function playround(cp_choice, human_choice) {
+  document.getElementById("displayresult").innerHTML="";
   cp_choice = String(cp_choice).toLowerCase();
   human_choice = String(human_choice).toLowerCase();
-
-  if (cp_choice === 'rock' && human_choice === 'rock') {
-    console.log("It's a tie");
-  } else if (cp_choice === 'rock' && human_choice === 'paper') {
-    console.log("Congrats, Paper beats Rock");
-    humanscore += 1;
-  } else if (cp_choice === 'rock' && human_choice === 'scissors') {
-    console.log("Oh you lost, Rock beats Scissors");
-    cpscore += 1;
-  } else if (cp_choice === 'paper' && human_choice === 'rock') {
-    console.log("Oh you lost, Paper beats Rock");
-    cpscore += 1;
-  } else if (cp_choice === 'paper' && human_choice === 'paper') {
-    console.log("It's a tie");
-  } else if (cp_choice === 'paper' && human_choice === 'scissors') {
-    console.log("Congrats, you won, Scissors beats Paper");
-    humanscore += 1;
-  } else if (cp_choice === 'scissors' && human_choice === 'rock') {
-    console.log("Congrats, you won, Rock beats Scissors");
-    humanscore += 1;
-  } else if (cp_choice === 'scissors' && human_choice === 'scissors') {
-    console.log("It's a tie");
-  } else if (cp_choice === 'scissors' && human_choice === 'paper') {
-    console.log("Oh you lost, Scissors beats Paper");
-    cpscore += 1;
-  } else {
-    console.log("Unwanted Error");
-  }
-}
-
-function playgame()
-{
   
-  let n = 5;
-  for(let i=0; i<n ; i++){
-
-    const human_selection = get_human();
-    const com_selection = get_cp();
-
-    playround(com_selection , human_selection);
+if(round<5){
+  if (cp_choice ===  human_choice ) {
+    document.getElementById("displayresult").innerHTML="Its a tie";
   }
-  console.log("Your score : ", humanscore , "Computer score : ", cpscore);
-
-  
-
+   else if (
+        (cp_choice === "rock" && human_choice === "scissors") ||
+        (cp_choice === "paper" && human_choice === "rock") ||
+        (cp_choice === "scissors" && human_choice === "paper")
+      ) {
+        document.getElementById("displayresult").innerHTML = "Computer wins!";
+        cpscore+=1;
+      } else {
+        document.getElementById("displayresult").innerHTML = "You win!";
+        humanscore+=1;
+      }
+      round++;
+    }
+    else if(round==5){
+      document.getElementById("displayresult").innerHTML = "Your score: " + humanscore + "Computer Score: " + cpscore;
+      round=0;
+      humanscore=0;
+      cpscore=0;
+    }
 }
 
 
-playgame();
+
